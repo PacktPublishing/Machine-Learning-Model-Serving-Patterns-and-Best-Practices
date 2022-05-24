d77defdd68a86626526d5c1061eaded243bcd59a
Md Johirul Islam <johirbuet@gmail.com>
2022-05-23 20:55:41 -0500

    Updates in the monitor module

--- Chapter 4/monitor.py
+++ Chapter 4/monitor.py
@@ -8,9 +8,13 @@ MAPE = np.array([0, 3, 8, 12, 20, 30])
 RMSE = np.array([0, math.sqrt(4000), math.sqrt(7000),
                  math.sqrt(11000), math.sqrt(15000), math.sqrt(23000)])
 
+MSE_Thresholds = np.array([5000, 5000, 5000, 5000, 5000, 5000])
+
 fig, ax = plt.subplots(2, 2)
 fig.suptitle("Model monitoring dashboard")
 ax[0, 0].plot(MSE)
+ax[0, 0].plot(MSE_Thresholds, 'r--', label="MSE Threshold")
+ax[0, 0].legend()
 ax[0, 0].set(xlabel="Index of Month", ylabel="MSE")
 
 ax[0, 1].plot(MAE)
@@ -24,4 +28,8 @@ ax[1, 1].set(xlabel="Index of Month", ylabel="RMSE")
 
 plt.show()
 
+
+MSE_Threshold = 5000
+if MSE[-1] >= MSE_Threshold:
+    print("Performance has dropped beyond the accepted threshold")
 
